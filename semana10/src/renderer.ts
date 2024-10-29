ce8758160b6ccda07c2f0c7d4f0f5b04bfe262c2
ROSANE MAIA BETINE <rosane_maia@estudante.sc.senai.br>
2024-10-28 21:51:15 -0300

    semana10

---
+++ semana10/src/renderer.ts
@@ -0,0 +1,59 @@
+
+
+import veiculo from './entity/veiculo';
+import './index.css';
+
+var listaVeiculos :veiculo[] = [];
+
+document.getElementById("botao-cadastrar")?.addEventListener("click", (event:MouseEvent) => {
+    event.preventDefault();
+
+    var modelo = document.getElementById("modelo") as HTMLInputElement;
+    var cor = document.getElementById("cor")as HTMLInputElement;
+    var ano = document.getElementById("ano")as HTMLInputElement;
+    var preco = document.getElementById("preco")as HTMLInputElement;
+    var placa = document.getElementById("placa")as HTMLInputElement;
+    var imagem = document.getElementById("imagem")as HTMLInputElement;
+
+    // const novoCarro = {
+    //     modelo: modelo.value,
+    //     cor: cor.value,
+    //     ano: ano.value,
+    //     preco: preco.value, 
+    //     placa: placa.value,
+    //     imagem: imagem.value,
+    // }
+
+    const novoVeiculo = new veiculo (modelo.value,cor.value,Number(ano.value),Number(preco.value),placa.value,imagem.value);
+
+    listaVeiculos.push(novoVeiculo);
+    (window as any).bancoAPI.createVeiculo(novoVeiculo)
+    
+    const lista_campos = ["modelo","cor","ano","preco","placa","imagem"];
+    lista_campos.forEach((campo) => (document.getElementById(campo) as HTMLInputElement).value = "");
+
+    var aside = document.getElementById("lista_veiculo");
+    aside.innerHTML = "";
+
+    for(var i = 0; i < listaVeiculos.length; i++){
+        aside.innerHTML += `
+        <div class="card">
+        <img src="${listaVeiculos[i].getImagem()}" alt="" srcset="">
+        <div class="dados">
+          <strong>${listaVeiculos[i].getModelo()}</strong>
+          <span>cor${listaVeiculos[i].getCor()}</span>
+          <span>ano${listaVeiculos[i].getAno()}</span>
+          <span>preco${listaVeiculos[i].getPreco()}</span>
+          <span>placa${listaVeiculos[i].getPlaca()}</span>
+        </div>
+        <div class="botao-card">
+          <button id="botao-ver">ver</button>
+          <button id="botao-deletar">deletar</button>
+        </div>
+      </div>
+     ` ;
+    }
+    
+    
+})
+
